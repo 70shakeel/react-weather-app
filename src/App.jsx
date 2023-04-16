65fa2260139ea2d1323eff70c25e2d340c1da3c1
Shakeel <70shakeel@gmail.com>
2023-04-16 18:00:09 +0500

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Button, TextField, Box } from "@mui/material";
+
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  const [city, setCity] = useState("Islamabad");
+  const [weather, setWeather] = useState("Islamabad");
+  const handleSearch = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <form onSubmit={handleSearch}>
+        <TextField
+          id="standard-basic"
+          label="Type city name..."
+          variant="standard"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onBlur={(e) => setWeather(e.target.value)}
+        />
+
+        <Button variant="contained">Search</Button>
+      </form>
+
+      <Weather city={weather} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+
+const Weather = ({ city }) => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const API_KEY = "758eca4e1449d0f2f2f208aea6b0dca0";
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [city]);
+  console.log(weatherData);
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, main, weather } = weatherData;
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff0",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+  return (
+    <div>
+      <h2>Current weather for {name}</h2>
+
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid xs={12}>
+            <p>
+              <img
+                src={`https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}
+                alt="weather icon"
+              />
+              <br /> {weather[0].description}
+            </p>
+          </Grid>
+          <Grid xs={6}>
+            <p>
+              Temperature
+              <br /> {main.temp}°C
+            </p>
+          </Grid>
+          <Grid xs={6}>
+            <p>
+              Feels like
+              <br /> {main.feels_like}°C
+            </p>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+};
+
+export default Weather;
